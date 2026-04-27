0a6655b9753b74b0fa374a8e57dad031ecfc2ff5
Muhammed Kasim TP <tpkasimvlr@gmail.com>
2026-04-27 21:43:32 +0530

    awate funtion

---
+++ src/main.js
@@ -0,0 +1,129 @@
+
+render();
+function render(){
+    let list = document.getElementById("taskList");
+  list.innerHTML = ""; 
+
+  let todos = JSON.parse(localStorage.getItem('todos')) || [];
+   todos.forEach((text, index) => {
+
+    let li = document.createElement("li");
+  li.className = "flex justify-between items-center bg-yellow-100 p-3 rounded";
+
+
+  let div = document.createElement("div");
+  div.className="flex items-center gap-2 w-full";
+  // div.classList.add("flex-1");
+
+  let checkbox = document.createElement("input");
+  checkbox.type="checkbox";
+
+  let span = document.createElement("span");
+  span.innerText =  text;
+
+ checkbox.onclick =function(){
+    if (checkbox.checked){
+      span.classList.add("line-through");
+    
+     
+    }
+    
+     else{
+      span.classList.remove("line-through");
+    
+     }
+     
+         localStorage.setItem("todos", JSON.stringify(todos)); 
+  
+         
+  };
+  
+
+  div.appendChild(checkbox);
+  div.appendChild(span);
+  li.appendChild(div);
+ 
+  
+  let button = document.createElement("button");
+  button.innerText="❌";
+  button.className="ml-2";
+
+  button.onclick = function(){
+    todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    render(); 
+  };
+  
+
+  li.appendChild(button);
+
+  
+  let edit = document.createElement("button");
+  edit.innerText="✏️";
+  edit.className="ml-2";
+
+  edit.onclick = function(){
+
+    let inbox = document.createElement("input");
+    inbox.className="border px-2 py-1 rounded w-full";
+    inbox.value=span.innerText;
+
+    div.replaceChild(inbox, span);
+    inbox.focus();
+
+    inbox.addEventListener("keydown", function(e){
+      if(e.key === "Enter"){
+        todos[index] = inbox.value;
+        localStorage.setItem("todos", JSON.stringify(todos));
+        render(); 
+      }
+    });
+
+  };
+
+  li.appendChild(edit);
+
+  document.getElementById("taskList").appendChild(li);
+})
+
+
+};
+
+ function delay(){
+  return new Promise((resolve) => {
+    setTimeout(() => {
+    resolve();
+    },2000);
+    
+  });
+}
+
+
+async function addTask(){
+  let input = document.getElementById("taskInput");
+  let text = input.value; 
+  if(text === "") return; 
+
+  let list =document.getElementById("taskList");
+  list.innerHTML= "loading";
+  
+  await delay();
+
+   let todos = JSON.parse(localStorage.getItem('todos')) || [];
+  
+  todos.push(text);
+
+
+
+
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  input.value = "";
+
+  render(); 
+}
+
+
+
+
---
+++ todo.js
@@ -0,0 +1,116 @@
+
+
+
+function fetchAllTodos() {
+  fetch('https://dummyjson.com/todos')
+    .then(res => res.json())
+    .then(data => {
+      todos = data.todos
+      renderTodos();
+    });
+}
+
+function renderTodos() {
+  let list = document.getElementById("taskList");
+  list.innerHTML = "";
+
+  todos.forEach((text, index) => {
+
+    let li = document.createElement("li");
+    li.className =
+      "flex justify-between items-center bg-yellow-100 p-3 rounded";
+
+    let div = document.createElement("div");
+    div.className = "flex items-center gap-2 w-full";
+
+    // checkbx
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = text.completed;
+
+    let span = document.createElement("span");
+    span.innerText = text.todo;
+
+    if (text.completed) {
+      span.classList.add("line-through");
+    }
+
+    checkbox.onclick = function () {
+      span.classList.toggle("line-through");
+     
+    };
+
+    div.appendChild(checkbox);
+    div.appendChild(span);
+
+
+
+
+
+
+    // editttt
+
+
+
+    
+    let editBtn = document.createElement("button");
+    editBtn.innerText = "✏️";
+
+    editBtn.onclick = function () {
+      let input = document.createElement("input");
+      input.value = text.todo;
+      input.className = "border px-2";
+
+      div.replaceChild(input, span);
+      input.focus();
+
+      input.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+          text.todo = input.value;
+          span.innerText = input.value;
+          div.replaceChild(span, input);
+
+          
+          fetch(`https://dummyjson.com/todos/${text.id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              todo: input.value,
+            }),
+          })
+          .then(res => res.json())
+          .then(console.log);
+        }
+      });
+    };
+
+
+
+
+
+    // delete 
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "❌";
+
+    delBtn.onclick = function () {
+      todos.splice(index, 1);
+      renderTodos();
+
+      
+      fetch(`https://dummyjson.com/todos/${text.id}`, {
+        method: "DELETE",
+      })
+      .then(res => res.json())
+      .then(console.log);
+    };
+
+    li.appendChild(div);
+    li.appendChild(editBtn);
+    li.appendChild(delBtn);
+
+    list.appendChild(li);
+  });
+}
+
+
+fetchAllTodos();
